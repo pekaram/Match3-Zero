4ec1be7fd2d43fb07e205ff5871e08aa600ee046
pekaram <petertalaat@hotmail.com>
2023-02-17 13:07:05 +0200

    Change struct to readonly

--- Assets/Scripts/Slot/SlotIndex.cs
+++ Assets/Scripts/Slot/SlotIndex.cs
@@ -1,8 +1,8 @@
-public struct SlotIndex
+public readonly struct SlotIndex
 {
-    public readonly int Row { get; }
+    public int Row { get; }
 
-    public readonly int Column { get; }
+    public int Column { get; }
 
     public SlotIndex(in int row, in int column)
     {
